dd5bee019eae7d925085a74634752269a74fbc01
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2026-03-17 17:29:43 +0530

    Contact section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Cocktails from "./components/Cocktails"
 import About from "./components/About"
 import Art from "./components/Art"
 import Menu from "./components/Menu"
+import Contact from "./components/Contact"
 gsap.registerPlugin(ScrollTrigger,SplitText) //Both plugins can now be used globally in our application
 // Split Text allow us to break text into individual characters for
 //detailed text animation 
@@ -22,6 +23,7 @@ export const App = () => {
       <About />
       <Art/>
      <Menu/>
+     <Contact />
     </main>
   )
 }
